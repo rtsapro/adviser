47765a6af0552e31742bc5aa235bef22d3323666
Romich <rtsapro@gmail.com>
2017-10-01 19:51:17 +0300

    add client entity

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Client;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,9 +15,22 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $client = new Client();
+        $client->setName('test');
+        $client->setKey(strtoupper(substr(uniqid(), 0, 12)));
+        $client->setApiVersion('1.0');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($client);
+        $em->flush();
+
+        $client = $em->getRepository('AppBundle:Client')
+            ->find($client->getId());
+        $client->setApiVersion('1.1');
+
+        $em->persist($client);
+        $em->flush();
+
+        return new Response("Ok !!!");
     }
 }
---
+++ src/AppBundle/Entity/Client.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use \Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="client")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Client
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=12, name="`key`")
+     */
+    private $key;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $apiVersion;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $modifiedIn;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string $key
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiVersion()
+    {
+        return $this->apiVersion;
+    }
+
+    /**
+     * @param string $apiVersion
+     */
+    public function setApiVersion($apiVersion)
+    {
+        $this->apiVersion = $apiVersion;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     */
+    public function setCreatedAt()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @return string
+     */
+    public function getModifiedIn()
+    {
+        return $this->modifiedIn;
+    }
+
+    /**
+     * @ORM\PreUpdate()
+     */
+    public function setModifiedIn()
+    {
+        $this->modifiedIn = new \DateTime();
+    }
+}
