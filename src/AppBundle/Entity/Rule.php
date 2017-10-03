761ce5631f298fbd0e3b761e31ebf098b212e769
Romich <rtsapro@gmail.com>
2017-10-03 18:04:17 +0300

    add rule entity

---
+++ src/AppBundle/Entity/Rule.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Rule
+ * @package AppBundle\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="rule")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Rule
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $className;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $params;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $modifiedIn;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return $this
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClassName()
+    {
+        return $this->className;
+    }
+
+    /**
+     * @param string $className
+     * @return $this
+     */
+    public function setClassName($className)
+    {
+        $this->className = $className;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    /**
+     * @param string $params
+     * @return $this
+     */
+    public function setParams($params)
+    {
+        $this->params = $params;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @return $this
+     */
+    public function setCreatedAt()
+    {
+        $this->createdAt = new \DateTime();
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getModifiedIn()
+    {
+        return $this->modifiedIn;
+    }
+
+    /**
+     * @ORM\PreUpdate
+     * @return $this
+     */
+    public function setModifiedIn()
+    {
+        $this->modifiedIn = new \DateTime();
+        return $this;
+    }
+}
